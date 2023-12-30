d81ab6e54973c751aa3f9e49bbc8ab17b895a725
volynvlad <volynvlad@gmail.com>
2023-12-31 00:19:22 +0100

    Add tests for sparse matrix

--- my_topic_modeling/sparse_matrix.py
+++ my_topic_modeling/sparse_matrix.py
@@ -1,26 +1,49 @@
 import numpy as np
 
-from logger import logger
-
 
 class SparseMatrix:
     def __init__(self, matrix: np.ndarray | None = None) -> None:
         self.values = {}
         self.columns = set() 
         self.rows = set()
-        if matrix:
+        if matrix is not None:
             n_columns, n_rows = matrix.shape
-            self.columns = set(range(n_columns))
-            self.rows = set(range(n_rows))
             for col in range(n_columns):
                 for row in range(n_rows):
                     self.add_element(col, row, matrix[col, row])
+
+    def __str__(self) -> str:
+        result = ""
+        for key in self.values:
+            result += f"{key}: {self.values[key]}\n"
+        return result
+
+    def __setitem__(self, index, value):
+        self.add_element(index[0], index[1], value)
+
+    def __getitem__(self, index):
+        return self.values[index]
+
+    def __eq__(self, compare_matrix) -> bool:
+        if not isinstance(compare_matrix, SparseMatrix):
+            return False
+        if not (self.columns == compare_matrix.columns and self.rows == compare_matrix.rows):
+            return False
+        if set(self.values.keys()) != set(compare_matrix.values.keys()):
+            return False
+        for key in self.values:
+            if self.values[key] != compare_matrix.values[key]:
+                return False
+        return True
 
     def add_element(self, col: int, row: int, value: float):
         if value:
             self.columns.add(col)
             self.rows.add(row)
-            self.values[(col, row)] = value
+            if (col, row) in self.values:
+                self.values[(col, row)] += value
+            else:
+                self.values[(col, row)] = value
 
     def remove_element(self, col: int, row: int):
         if col in self.columns and row in self.rows:
@@ -31,8 +54,16 @@ class SparseMatrix:
             raise IndexError(f"there is no value in ({col}, {row}) element")
 
 
+
 def multiply_sp_matrix(
     left_matrix: SparseMatrix,
     right_matrix: SparseMatrix,
     ):
-    return SparseMatrix()
+    result_matrix = SparseMatrix()
+    for lm in left_matrix.values:
+        for rm in right_matrix.values:
+            if lm[1] == rm[0]:
+                result_matrix[(lm[0], rm[1])] = left_matrix[lm] * right_matrix[rm]
+
+    return result_matrix
+
